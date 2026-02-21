effbf3742eb9afeec4f9c905ef4f144064eb70c7
Md. Ashikul Hoque <121496659+ashiksaad172150@users.noreply.github.com>
2026-02-21 22:27:07 +0600

    Time: 32 ms (36.70%) | Memory: 54.2 MB (84.19%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,11 @@
+const twoSum=function(arr,target) {
+    let i,j;
+    for(i=0;i<arr.length;i++) {
+        for(j=i+1;j<arr.length;j++) {
+            if((arr[i]+arr[j])===target) {
+                return [i,j];
+            } 
+        }
+    }
+    return [];
+};
